5c541ca9816c119223c632b736e15bfc830d452c
Davide "5KUBY" Pala <skullb0y@protonmail.ch>
2020-08-29 19:40:50 +0200

    End search time reference added
    added the possibility to specify an end search time reference in order to search until it.

--- logextractor.sh
+++ logextractor.sh
@@ -9,7 +9,8 @@ echo "insert log search end time in hh:mm:ss format:"
 read end_time
 
 #When a log file is archived SNS add a string to filename with time references in the format yyyymmddhhmmss the following instruction adapt start time to this format
-normalized_time=$(echo "$start_time" | awk -F: '{print $1$2$3}')
+SNS_logformat_start_time=$(echo "$start_time" | awk -F: '{print $1$2$3}')
+SNS_logformat_end_time=$(echo "$end_time" | awk -F: '{print $1$2$3}')
 
 #The following code is to ask log type, ip addresses and time frame
 echo "Select log type:
@@ -38,22 +39,24 @@ echo "insert output file name with path (example: /log/myoutfile):"
 read outfile
 
 #start_time_refenrence is composed by date and time var and is used to find log files which can contain logs we are looking for
-start_time_reference=$start_date$normalized_time
-
+start_time_reference=$start_date$SNS_logformat_start_time
+end_time_reference=$end_date$SNS_logformat_end_time
 
 #This for loop search in all logs of the type specified during the wizard 
 #but execute a grep instrucion only against logs which have a close reference more recent 
 #than the time reference given in the wizard 
 for logfile in $(ls /log/l_$log_type*)
 do
- filedate=$(echo $logfile | cut -f5 -d'_') #cut extract the 5th field (close time) from a log file name using underscore as separator
- if [ $logfile == /log/l_connection ] #this is set filedate to 99999999999999 in order to include l_connection file in the search in case of absence of search extended to the actual time
+ log_closing_reference=$(echo $logfile | cut -f5 -d'_') #cut extract the 5th field (close time) from a log file name using underscore as separator
+ log_opening_reference=$(echo $logfile | cut -f4 -d'_') #
+ if [ $logfile == /log/l_connection ] #this is set log_closing_reference to 99999999999999 in order to include l_connection file in the search in case of absence of search extended to the actual time
   then
-   filedate=99999999999999
+   log_closing_reference=99999999999999
+   log_opening_reference=00000000000000
  fi
- if [ $filedate -ge $start_time_reference ] # if filedate (log close time) is greater or equal to the start time reference it means that the file can contain logs generated in the search time frame 
+ if [ $log_closing_reference -ge $start_time_reference -a $log_opening_reference -le $end_time_reference ]; # if log_closing_reference (log close time) is greater or equal to the start time reference it means that the file can contain logs generated in the search time frame 
   then
-   echo "grep in $logfile"
+    echo "grep in $logfile"
    grep -E $srcip'\>' $logfile | grep -E $dstip'\>' >> /tmp/log_extractor_tempfile #execute a grep with extended expression and write the result in a tmp file 
  fi
 done
@@ -66,7 +69,7 @@ done
 while read line 
  do
   line_ref=$(echo $line| cut -f2,3 -d' '| sed 's/[^0-9]*//g')
-  if [ $line_ref -ge $start_time_reference ]
+  if [ $line_ref -ge $start_time_reference -a $line_ref -le $end_time_reference ];
    then
     echo "$line" >> $outfile
   fi
